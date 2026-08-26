b7444c7cfc2a59cb149e58b4254a67b346afb914
mayank-sherawat <mayanksherawat21@gmail.com>
2026-08-26 21:46:42 +0530

    fix: remove deprecated middleware.ts in favor of proxy.ts

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { withAuth } from "next-auth/middleware";
-
-export default withAuth({
-  pages: { signIn: "/login" },
-});
-
-export const config = {
-  matcher: ["/feed/:path*", "/profile/:path*", "/settings/:path*", "/search/:path*"],
-};
